dcbafdd9b09eb664c2ae21307ce082eaf01fddd8
MD SAZZAD HOSSAIN SOHAG <md.sazzad.ft24@mail.umy.ac.id>
2025-03-17 20:20:07 +0700

    add function to create circle area

--- switchcase.cpp
+++ switchcase.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+float rectangleArea(float length, float width) {
+    return length * width;
+}
+
+float circleArea(float radius) {
+    return 3.14 * radius * radius;
+}
+
