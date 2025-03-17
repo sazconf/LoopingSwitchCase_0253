14fb53caece8db52f6b0d8878dff316e070800ba
MD SAZZAD HOSSAIN SOHAG <md.sazzad.ft24@mail.umy.ac.id>
2025-03-17 19:46:58 +0700

    add for loops

--- loop.cpp
+++ loop.cpp
@@ -3,3 +3,36 @@
 #include <cstdlib>
 using namespace std;
 
+int main()
+{
+    int n;
+    int whileLoopCount = 0;
+    int doLoopCount = 0;
+
+    // Loop with for
+    cout << "Ascending loop with for" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "n value = " << n << " Welcome" << endl;
+    }
+    cout << "Final n value = " << n << endl;
+    cout << endl;
+
+    cout << "Descending loop with for" << endl;
+    // Loop with for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "n value = " << n << " Welcome" << endl;
+    }
+    cout << "Final n value = " << n << endl;
+    cout << endl;
+
+    cout << "Loop with while" << endl;
+    srand(time(0));
+
+    n = rand() % 10;
+
+    cout << "Initial n value = " << n << endl;
+
+  
+}
